aa6fa973f179d5c8a8af14390b91f278592d164f
vishnups678 <vishnusasi18@gmail.com>
2025-03-09 00:40:20 +0530

    initial commit

---
+++ src/app/layouts/full/sidebar/branding.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CoreService } from 'src/app/services/core.service';
+
+@Component({
+  selector: 'app-branding',
+  imports: [],
+  template: `
+    <a href="/" class="logodark">
+      <!-- <img
+        src="./assets/images/logos/dark-logo.svg"
+        class="align-middle m-2"
+        alt="logo"
+      /> -->
+      Seek and Find
+    </a>
+  `,
+})
+export class BrandingComponent {
+  options = this.settings.getOptions();
+  constructor(private settings: CoreService) {}
+}
